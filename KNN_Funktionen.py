4786124e41d874d8c910542ce14e235eee0925b9
Sammy-Jankins <46397761+Sammy-Jankins@users.noreply.github.com>
2019-04-29 14:10:07 +0200

    Create KNN_Funktionen.py

---
+++ KNN_Funktionen.py
@@ -0,0 +1,66 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Ein Input-Neuron
+Anzahl_verborgene_Neuronen = 4
+Bias = "Ja" # mögliche Werte: "Ja", "Nein"
+Aktivierungsfunktion = "ReLU" # mögliche Werte: "ReLU", "Linear"
+Input_Gewichte = [1,1,2,8]#[-1,2]#[1,1,2,8]
+Bias_Gewichte = [0,1,-2,-16]#[0,-2]#[0,1,-2,-16]
+Output_Gewichte = [1,1,1,1]
+
+Daten = [Anzahl_verborgene_Neuronen, Bias, Aktivierungsfunktion, Input_Gewichte, Bias_Gewichte, Output_Gewichte]
+
+def Act(x):
+    if Aktivierungsfunktion == "ReLU":
+        return x * (x > 0)
+    elif Aktivierungsfunktion == "Linear":
+        return x
+    else:
+        raise ValueError("Aktivierungsfunktion falsch definiert.")
+    
+def KNN_Funktion(x, Daten):
+    [Anzahl_verborgene_Neuronen, Bias, Aktivierungsfunktion, Input_Gewichte, Bias_Gewichte, Output_Gewichte] = Daten
+    Zustand = []
+    for j in range(Anzahl_verborgene_Neuronen):
+        Zustand.append(Output_Gewichte[j] * Act(Input_Gewichte[j] * x + Bias_Gewichte[j]))
+    return np.sum(Zustand)
+
+def Graph_KNN_Funktion(Daten, links, rechts):
+    x_Werte = np.linspace(-1,2,100)
+    y_Werte = []
+    for j in range(len(x_Werte)):
+        y_Werte.append(KNN_Funktion(x_Werte[j], Daten))
+    
+    plt.plot(x_Werte, y_Werte, color='blue', label='KNN-Funktion')
+    plt.legend()
+    plt.title('KNN-Funktion')
+    plt.show()  
+
+def Graph_KNN_Funktion_Expo(Daten):
+    x_Werte = np.linspace(-3,3,100)
+    y_Expo = np.exp(x_Werte)
+    y_Werte = []
+    for j in range(len(x_Werte)):
+        y_Werte.append(KNN_Funktion(x_Werte[j], Daten))
+    
+    plt.plot(x_Werte, y_Werte, color='blue', label='KNN-Funktion')
+    plt.plot(x_Werte, y_Expo, color='red', label='Exponentialfunktion')
+    plt.legend()
+    plt.title('KNN-Funktion und Exponentialfunktion')
+    plt.show()  
+    maxi = 0
+
+    for j in range(len(x_Werte)):
+        maxi = max(maxi, np.abs(y_Werte[j]-y_Expo[j]))
+    print("Der Approximationsfehler beträgt: " + str("{:.4f}".format(maxi)))
+    if maxi < 2:
+        print("Der Fehler ist < 2, Glückwunsch!")
+    else:
+        print("Leider ist der Fehler > 2 :(")
+    
+
+Graph_KNN_Funktion_Expo(Daten)
+
+
+    
